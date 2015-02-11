9880b6d80730a75d3e044cfc7314b12481f30d5e
rango <rango@rango-2.local>
2015-02-11 11:41:53 +0800

    start create
    项目刚刚开始创建

---
+++ Aima/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Aima
+//
+//  Created by rango on 15/2/11.
+//  Copyright (c) 2015年 rango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Aima/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Aima
+//
+//  Created by rango on 15/2/11.
+//  Copyright (c) 2015年 rango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
